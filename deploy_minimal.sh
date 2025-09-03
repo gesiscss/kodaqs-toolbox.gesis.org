0f66385749f2d565ff8a14b35a06ac46769db9e2
Jun S <yfiua6@gmail.com>
2025-09-03 16:36:53 +0200

    no need to deploy minimal version

--- deploy_minimal.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd ./minimal_example || exit
-sudo mkdir -p /var/www/minimal
-sudo rm -rf /var/www/minimal/*
-sudo cp -r ./minimal_example/_site/* /var/www/minimal/
-sudo chown -R www-data:www-data /var/www/minimal/*
-sudo chmod -R 755 /var/www/minimal/*
-sudo systemctl restart nginx
