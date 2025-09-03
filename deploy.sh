aed9213cb0dad33659b8ad4d158caed5709d64f8
Jun S <yfiua6@gmail.com>
2025-09-03 16:37:21 +0200

    setting correct permissions for web files; partially #47

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,6 @@ cd ./demo || exit
 sudo rm -rf /var/www/html/*
 sudo cp -r _site/* /var/www/html/
 sudo chown -R www-data:www-data /var/www/html/*
-sudo chmod -R 755 /var/www/html/*
+sudo find /var/www/html/ -type f -print0 | xargs -0 chmod 0644
+sudo find /var/www/html/ -type d -print0 | xargs -0 chmod 0755
 sudo systemctl restart nginx
--- deploy_test.sh
+++ deploy_test.sh
@@ -5,5 +5,6 @@ sudo mkdir -p /var/www/test
 sudo rm -rf /var/www/test/*
 sudo cp -r _site/* /var/www/test/
 sudo chown -R www-data:www-data /var/www/test/*
-sudo chmod -R 755 /var/www/test/*
+sudo find /var/www/test/ -type f -print0 | xargs -0 chmod 0644
+sudo find /var/www/test/ -type d -print0 | xargs -0 chmod 0755
 sudo systemctl restart nginx
