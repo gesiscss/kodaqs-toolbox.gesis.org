039e1400a219c85b561418579f4cbdc08917dfee
Jun S <yfiua6@gmail.com>
2025-11-27 22:18:02 +0100

    remove unnecessary sudo

--- prepare_update.sh
+++ prepare_update.sh
@@ -1,5 +1,5 @@
 docker images --format '{{.Repository}}:{{.Tag}} {{.ID}}' | grep '^andrew/' | awk '{print $2}' | xargs docker rmi -f
-sudo rm -f demo/_github
-sudo rm -f demo/github
-sudo rm -f minimal_example/_github
-sudo rm -f minimal_example/github
+rm -f demo/_github
+rm -f demo/github
+rm -f minimal_example/_github
+rm -f minimal_example/github
--- prepare_update_minimal.sh
+++ prepare_update_minimal.sh
@@ -1,3 +1,3 @@
 docker images --format '{{.Repository}}:{{.Tag}} {{.ID}}' | grep '^andrew/' | awk '{print $2}' | xargs docker rmi -f
-sudo rm -f minimal_example/_github
-sudo rm -f minimal_example/github
+rm -f minimal_example/_github
+rm -f minimal_example/github
