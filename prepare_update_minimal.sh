f6cf6b3278cebe8a3dc79126d9156f843ab26505
Jun S <yfiua6@gmail.com>
2025-11-27 21:50:49 +0100

    Stop setting everything to 777; partially #47

--- prepare_update_minimal.sh
+++ prepare_update_minimal.sh
@@ -1,7 +1,3 @@
-sudo chmod -R 777 .
 docker images --format '{{.Repository}}:{{.Tag}} {{.ID}}' | grep '^andrew/' | awk '{print $2}' | xargs docker rmi -f
-sudo chmod -R 777 .
 sudo rm -f minimal_example/_github
 sudo rm -f minimal_example/github
-sudo chmod -R 777 .
-
